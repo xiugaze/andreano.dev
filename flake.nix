dfd97f4f87b8de878a7581578f53fe4c63dab03b
Caleb Andreano <calebandreano@gmail.com>
2025-04-11 13:35:05 -0500

    idk

--- flake.nix
+++ flake.nix
@@ -5,8 +5,15 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
@@ -29,7 +36,7 @@
         #   src = pkgs.fetchurl {
         #     url = "https://github.com/xiugaze/andreano.dev/releases/download/v0.1.0-alpha/site";
         #   };
-        #   dontUnpack = true; 
+        #   dontUnpack = true;
         #   buildPhase = ''
         #       cp $src site
         #     '';
@@ -40,100 +47,119 @@
         #     '';
         # };
 
-    in {
-      packages.default = andreano-dev-package;
-      packages.andreano-dev-package = andreano-dev-package;
-      packages.andreano-dev-site = pkgs.runCommand "andreano-dev-site" {
-        buildInputs = [ self.packages.${system}.default pkgs.git ];
-      } ''
-          mkdir -p $out
-          cp -r ${./site/website} ./website
-          chmod -R u+w ./website
-          cd ./website
-          ${self.packages.${system}.default}/bin/site crunch ${toString self.shortRev or self.dirtyShortRev}
-          cp -r . $out/  # Copy the entire contents of ./website to $out
-        '';
+      in
+      {
+        packages.default = andreano-dev-package;
+        packages.andreano-dev-package = andreano-dev-package;
+        packages.andreano-dev-site =
+          pkgs.runCommand "andreano-dev-site"
+            {
+              buildInputs = [
+                self.packages.${system}.default
+                pkgs.git
+              ];
+            }
+            ''
+              mkdir -p $out
+              cp -r ${./site/website} ./website
+              chmod -R u+w ./website
+              cd ./website
+              ${self.packages.${system}.default}/bin/site crunch ${toString self.shortRev or self.dirtyShortRev}
+              cp -r . $out/  # Copy the entire contents of ./website to $out
+            '';
 
-
-      nixosModules.default = { config, lib, pkgs, ...}:
-          let 
+        nixosModules.default =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          let
             cfg = config.services.andreano-dev;
-          in with lib; {
-        options = {
-          services.andreano-dev = {
-            enable = mkOption {
-              default = false;
-              description = "enable the web server";
-            } ;
-            user = mkOption {
-              default = "andreano-dev";
-              description = "user to run the service";
-            };
-          };
-        };
-
-        config = mkIf config.services.andreano-dev.enable {
-
-          users.users.${cfg.user} = {
-            isSystemUser = true;
-            group = "${cfg.user}"; 
-            home = "/var/lib/andreano-dev"; 
-            createHome = true; 
-            description = "andreano-dev web service user";
-          };
-
-          users.groups.${cfg.user} = {};
-
-          systemd.services.andreano-dev = let
-            package-base = "${self.packages.${pkgs.system}.default}";
-            serve-path = "${self.packages.${pkgs.system}.andreano-dev-site}/static";
-            db-dir = "/var/lib/andreano-dev";
-          in {
-            wantedBy = [ "default.target" ]; 
-            after = [ "network.target" ];
-            serviceConfig = {
-              Type = "simple";
-              User = "${cfg.user}";
-              ExecStart = "${package-base}/bin/site serve ${serve-path}";
-            };
-          };
-        };
-      };
-      devShells.default = with pkgs; mkShell.override { stdenv = clangStdenv; } {
-          buildInputs = with pkgs; [
-            llvmPackages.libclang
-            llvmPackages.clang
-          ];
-
-          LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-          BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
-          CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld"; 
-
-          packages =
-            with pkgs;
-            let
-              rust = pkgs.rust-bin.stable.latest.default.override {
-                targets = [ "wasm32-unknown-unknown" ];
+          in
+          with lib;
+          {
+            options = {
+              services.andreano-dev = {
+                enable = mkOption {
+                  default = false;
+                  description = "enable the web server";
+                };
+                user = mkOption {
+                  default = "andreano-dev";
+                  description = "user to run the service";
+                };
               };
-            in
-            [
-              # rust
-              rust
-              rust-analyzer
-              wasm-pack
-              cmake
-              python3
-              sqlite
-              lld
+            };
 
-              cargo-flamegraph
-              linuxKernel.packages.linux_latest_libre.perf
+            config = mkIf config.services.andreano-dev.enable {
 
-              # tools
-              libwebp
+              users.users.${cfg.user} = {
+                isSystemUser = true;
+                group = "${cfg.user}";
+                home = "/var/lib/andreano-dev";
+                createHome = true;
+                description = "andreano-dev web service user";
+              };
 
-              sqlite
+              users.groups.${cfg.user} = { };
+
+              systemd.services.andreano-dev =
+                let
+                  package-base = "${self.packages.${pkgs.system}.default}";
+                  serve-path = "${self.packages.${pkgs.system}.andreano-dev-site}/static";
+                  db-dir = "/var/lib/andreano-dev";
+                in
+                {
+                  wantedBy = [ "default.target" ];
+                  after = [ "network.target" ];
+                  serviceConfig = {
+                    Type = "simple";
+                    User = "${cfg.user}";
+                    ExecStart = "${package-base}/bin/site serve ${serve-path}";
+                  };
+                };
+            };
+          };
+        devShells.default =
+          with pkgs;
+          mkShell.override { stdenv = clangStdenv; } {
+            buildInputs = with pkgs; [
+              llvmPackages.libclang
+              llvmPackages.clang
             ];
-        };
-    });
+
+            LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+            BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+            CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+
+            packages =
+              with pkgs;
+              let
+                rust = pkgs.rust-bin.stable.latest.default.override {
+                  targets = [ "wasm32-unknown-unknown" ];
+                };
+              in
+              [
+                # rust
+                rust
+                rust-analyzer
+                wasm-pack
+                cmake
+                python3
+                sqlite
+                lld
+
+                cargo-flamegraph
+                linuxKernel.packages.linux_latest_libre.perf
+
+                # tools
+                libwebp
+
+                sqlite
+              ];
+          };
+      }
+    );
 }
